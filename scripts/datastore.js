6c62a74d622ae6ba0272e340089ba93fba912ea2
Muki Dev <ibrahimfofana.mutaki0107@gmail.com>
2023-09-06 23:14:44 +0000

    Promise-ifying the other DataStore methods

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -8,23 +8,27 @@
         this.data = {};
     }
 
-    DataStore.prototype.add = function(key,val){
+    function promiseResolvedWith(value) {
         var promise = new Promise(function (resolve, reject) {
-            this.data[key] = val; }.bind(this));
-            resolve(null);
+          resolve(value);
+        });
         return promise;
+      }
+
+    DataStore.prototype.add = function(key,val){
+        return promiseResolvedWith(null);
     }
 
     DataStore.prototype.get = function(key){
-        return this.data[key];
+        return promiseResolvedWith(this.data[key]);
     }
 
     DataStore.prototype.getAll = function(){
-        return this.data;
+        return promiseResolvedWith(this.data);
     }
 
     DataStore.prototype.remove = function(key){
-        delete this.data[key];
+        return promiseResolvedWith(null);
     }
     App.DataStore = DataStore;
     window.App = App;
