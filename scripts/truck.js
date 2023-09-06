7d2095908faa091a26f1b1497287d909955290ef
Muki Dev <ibrahimfofana.mutaki0107@gmail.com>
2023-09-06 00:24:08 +0000

    Adding orders

--- scripts/truck.js
+++ scripts/truck.js
@@ -1,9 +1,15 @@
 (function(window){
 var App = window.App || {};
 
-function Truck(){
-
+function Truck(truckId,db){
+    this.truckId = truckId;
+    this.db = db;
 }
+
+Truck.prototype.createOrder = function (order) {
+    console.log('Adding order for ' + order.emailAddress);
+    this.db.add(order.emailAddress, order);
+  };
 
 App.Truck = Truck;
 window.App = App;
