ff26871bdd3ddaf7cb171f289558208bae6a9f29
Muki Dev <ibrahimfofana.mutaki0107@gmail.com>
2023-09-06 23:08:10 +0000

    Creating and returning Promises

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -1,13 +1,17 @@
 (function(window){
     'use strct';
     var App = window.App || {};
+    var Promise = window.Promise;
+
 
     function DataStore(){
         this.data = {};
     }
 
     DataStore.prototype.add = function(key,val){
-        this.data[key] = val;
+        var promise = new Promise(function (resolve, reject) {
+            this.data[key] = val; }.bind(this));
+        return promise;
     }
 
     DataStore.prototype.get = function(key){
