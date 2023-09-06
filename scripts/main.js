bb5b30643214c971bd7215cac86163d13b428f81
Muki Dev <ibrahimfofana.mutaki0107@gmail.com>
2023-09-06 17:26:23 +0000

    Manipulating this with call

--- scripts/main.js
+++ scripts/main.js
@@ -6,10 +6,15 @@
     var Truck = App.Truck;
     var DataStore = App.DataStore;
     var FormHandler = App.FormHandler;
+    var CheckList = App.CheckList;
     var myTruck = new Truck('ncc-1701', new DataStore());
     window.myTruck = myTruck;
+    var checkList = new CheckList(CHECKLIST_SELECTOR);
     var formHandler = new FormHandler(FORM_SELECTOR);
-    var CheckList = App.CheckList;
-  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  formHandler.addSubmitHandler(
+  function (data) {
+myTruck.createOrder.call(myTruck, data);
+checkList.addRow.call(checkList, data);
+})
   console.log(formHandler);
   })(window);
