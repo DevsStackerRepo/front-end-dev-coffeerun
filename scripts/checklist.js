d3bc54f8e8e60d7583612c2ffddd4ae338c9e7c7
Muki Dev <ibrahimfofana.mutaki0107@gmail.com>
2023-09-06 17:35:01 +0000

    Delivering an Order by Clicking a Row(Removing overwritten entries)

--- scripts/checklist.js
+++ scripts/checklist.js
@@ -13,6 +13,8 @@
   }
 
   CheckList.prototype.addRow = function (coffeeOrder) {
+     // Remove any existing rows that match the email address
+     this.removeRow(coffeeOrder.emailAddress);
     // Create a new instance of a row, using the coffee order info
     var rowElement = new Row(coffeeOrder);
     // Add the new row instance's $element property to the checklist
